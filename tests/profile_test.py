e5f4d247579e82cec1ea1723f405ec27b04c2aaf
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-03-26 01:08:35 +0000

    Profile tests more or less written

---
+++ tests/profile_test.py
@@ -0,0 +1,35 @@
+import unittest
+from src.profile import *
+from src.movie import *
+
+class TestProfile(unittest.TestCase):
+
+    def setUp(self):
+
+        # Creating some objects to use in the tests
+        self.movie_1 = Movie("The Fugitive", "Andrew Daivs", 9)
+        self.movie_2 = Movie("Hunger Games", "Gary Ross", 7)
+
+        self.profile_1 = Profile("harrisonF", "myP@assword")
+
+    # Test a Profile can add a favourite Movie
+    def profile_can_add_fav(self):
+        self.assertEqual(0, len(self.favourites))
+        self.profile.add_favourite(self.movie_1)
+        self.assertEqual([self.movie_1], self.favourites)
+
+    # Test a Profile can remove a given Movie from favourites
+    def profile_can_remove_fav(self):
+        self.assertEqual(0, len(self.favourites))
+        self.profile.add_favourite(self.movie_1)
+        self.profile.add_favourite(self.movie_2)
+        self.profile.remove_favourite(self.movie_1)
+        self.assertEqual([self.movie_2], self.favourites)
+
+    # Test a Profile can return a list of Favourites
+    def profile_returns_favs(self):
+        if self.profile.get_favourites() != None:
+            self.assertEqual(0, len(self.profile.get_favourites()))
+            self.profile.add_favourite(self.movie_1)
+            self.profile.add_favourite(self.movie_2)
+            self.assertEqual(2, len(self.profile.get_favourites())))
