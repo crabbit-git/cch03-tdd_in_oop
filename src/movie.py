2fa42c0141531b8e7d27949f3dac3218a6025af3
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-03-26 01:26:37 +0000

    Classes defined

---
+++ src/movie.py
@@ -0,0 +1,5 @@
+class Movie:
+    def __init__(self, title, director, rating):
+        self.title = title
+        self.director = director
+        self.rating = rating
---
+++ src/profile.py
@@ -0,0 +1,5 @@
+class Profile:
+    def __init__(self, username, password, favourites = []):
+        self.username = username
+        self.password = password
+        self.favourites = favourites
